327ecc5d1764ceb08a85ca2aa8d0db68d2b34d18
Leemu0822 <786102800@qq.com>
2019-08-30 16:43:51 +0800

    no message

---
+++ datastruct_/Assets/Stack/StackManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StackManager : MonoBehaviour
+{
+    [SerializeField] private Button delete_btn;
+    [SerializeField] private GameObject ball;
+    private Stack<GameObject>myStack=new Stack<GameObject>();
+	// Use this for initialization
+	void Start () {
+	    for (int i = 0; i < 5; i++)
+	    {
+	        GameObject go = Instantiate(ball, new Vector3(0, i, 0), gameObject.transform.rotation);
+            myStack.Push(go);
+	    }
+        delete_btn.onClick.AddListener(() =>
+        {
+            if (myStack.Count > 0)
+            {
+                GameObject top_go = myStack.Pop();
+                Destroy(top_go);
+            }
+           
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
