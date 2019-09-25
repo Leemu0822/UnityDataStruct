8d18b76905e41cef889b367dfcfc9ce7e1572d32
Leemu0822 <786102800@qq.com>
2019-09-25 17:34:32 +0800

    字典，散列表

---
+++ datastruct_/Assets/Dictionary/DictionaryExample.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DictionaryExample : MonoBehaviour
+{
+    [SerializeField] private InputField En_input;
+
+    [SerializeField] private Button search_btn;
+
+    [SerializeField] private Text cn_text;
+
+    private Dictionary<string,string>EnglishDict=new Dictionary<string, string>();
+	// Use this for initialization
+	void Start () {
+		EnglishDict.Add("below","在下面");
+        EnglishDict.Add("expert","专家");
+	    EnglishDict.Add("garnered","囊括");
+        search_btn.onClick.AddListener(() =>
+        {
+            if (EnglishDict.ContainsKey(En_input.text))
+            {
+                cn_text.text = EnglishDict[En_input.text];
+            }
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ datastruct_/Assets/HashTable/HashTableExample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashTableExample : MonoBehaviour {
+    Hashtable table = new Hashtable();
+
+    // Use this for initialization
+    void Start () {
+        //往哈希表添加元素
+        table.Add(1,10); 
+        table.Add("pp",99); 
+        table.Add('a',"66");
+
+        //取值
+        print(table["pp"]);
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+	}
+}
